4ab317d80da6a0094fa5053c2bfcfd57fcdf9d55
Raymond Ho <raymond@iquitsugar.com>
2018-01-25 23:00:06 +1100

    remove proptypes to fix error

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 'use strict';
 
-import React,{
-    PropTypes
-} from 'react';
+import React from 'react';
 
 import {
     View,
@@ -20,21 +18,21 @@ import BaseComponent from './BaseComponent';
 
 let componentIndex = 0;
 
-const propTypes = {
-    data: PropTypes.array,
-    onChange: PropTypes.func,
-    initValue: PropTypes.string,
-    style: View.propTypes.style,
-    selectStyle: View.propTypes.style,
-    optionStyle: View.propTypes.style,
-    optionTextStyle: Text.propTypes.style,
-    sectionStyle: View.propTypes.style,
-    sectionTextStyle: Text.propTypes.style,
-    cancelStyle: View.propTypes.style,
-    cancelTextStyle: Text.propTypes.style,
-    overlayStyle: View.propTypes.style,
-    cancelText: PropTypes.string
-};
+// const propTypes = {
+//     data: PropTypes.array,
+//     onChange: PropTypes.func,
+//     initValue: PropTypes.string,
+//     style: View.propTypes.style,
+//     selectStyle: View.propTypes.style,
+//     optionStyle: View.propTypes.style,
+//     optionTextStyle: Text.propTypes.style,
+//     sectionStyle: View.propTypes.style,
+//     sectionTextStyle: Text.propTypes.style,
+//     cancelStyle: View.propTypes.style,
+//     cancelTextStyle: Text.propTypes.style,
+//     overlayStyle: View.propTypes.style,
+//     cancelText: PropTypes.string
+// };
 
 const defaultProps = {
     data: [],
@@ -179,5 +177,5 @@ export default class ModalPicker extends BaseComponent {
     }
 }
 
-ModalPicker.propTypes = propTypes;
+// ModalPicker.propTypes = propTypes;
 ModalPicker.defaultProps = defaultProps;
